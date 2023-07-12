39284d5abb0c4d900252e91b6be176955dd50a13
Roee Toledano <86188707+Roeegg2@users.noreply.github.com>
2023-07-12 20:24:43 +0300

    preassembler-remake
    rewrote all the preassembler code.
    things to fix before can merge into main:
    1. program only works for 'source.as' and 'result.am' input and output file
    2. messy code on 'add_macro_codelines'
    
    NOTE:
    might be more things to fix that I just dont remember right now

--- preassembler.h
+++ preassembler.h
@@ -8,6 +8,7 @@
 #include <string.h>
 
 #define DELIMITERS " \t\n"
+#define MAX_LINE_LENGTH 82
 
 /*macro for freeing all the allocated space in the macros (the pointers attrabiutes and the pointer itself)*/
 #define FREE_ALL(macros) int j; for (j = 0; j < macroIndex+1; j++){free(macros[j].name); free(macros[j].code);} free(macros);
@@ -18,8 +19,6 @@
 typedef struct macro{
     char* name;
     char* code;
-} macro;
 
-int main_looper();
-int macro_code(char* line, FILE* amFile, macro* macros, int macroSize, int macroIndex);
-int is_macro(int macroIndex, macro* macros, char* word, FILE* amFile);
+    struct macro* next;
+} macro;

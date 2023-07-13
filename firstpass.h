35332a5f047a1db59173540171c5f598ad50fcab
Roee Toledano <86188707+Roeegg2@users.noreply.github.com>
2023-07-13 16:18:12 +0300

    improved code a bit and added first writing binary function
    commiting to have a backup.
    going to change the analyse_operands() to be compatible with parameteres with space in between.
    
    next commit should be (if there is nothing i need to save in the middle) a working first-pass, with good operation, labels, and parameters detection and perfect binary writing, need just to fix some error catching and other small things

--- firstpass.h
+++ firstpass.h
@@ -4,8 +4,10 @@
 
 #define MAX_LINE_LENGTH 82
 #define MAX_LABEL_LENGTH 31
+// check this and change later!
+#define MAX_DATA_LENGTH 10 
 #define DELIMITERS " \t\n"
-
+#define MAX_OPERANDS 2
 
 #define LAST_CHARACTER(string) *(string+strlen(string)-1)
 
@@ -20,3 +22,33 @@ typedef struct label{
     int type; /*the type of the table relocateable, */
     int counterType;
 } label;
+
+/* typedef union operand{
+    label* label1;
+    int registerNum;
+    int number;
+} operand;
+
+typedef union data{
+    int number;
+    char string[MAX_LINE_LENGTH];
+} data; */
+
+/* typedef struct instruction{
+    int opcode;
+    int sourceAddressingModes;
+    int destAddressingModes;
+} instruction;
+
+typedef union parameter{
+    union{
+        char labelName[MAX_LABEL_LENGTH];
+        int registerNum;
+        int number;
+    } operand;
+
+    union{
+        int number;
+        char string[MAX_DATA_LENGTH];
+    } data;
+} parameter; */

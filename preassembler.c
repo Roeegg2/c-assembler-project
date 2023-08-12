9af2f283e2250227faefd11acdcc35cde2c0f044
Roee Toledano <86188707+Roeegg2@users.noreply.github.com>
2023-08-12 21:04:37 +0300

    temp commit to not loose progress
    im doing a makeover to preassembler, so im saving this code for later reference

--- preassembler.c
+++ preassembler.c
@@ -8,16 +8,17 @@ int write_line(FILE** amFile, macro* head, char* token, char originalLine[], cha
         }
         head = head->next;
     }
+    
     fprintf(*amFile, "%s", originalLine);
     return 0;
 }
 
 /* make this piece of code nicer. */
-int add_macro_codelines(FILE** asFile, char* code, char line[], char originalLine[]){
+int add_macro_codelines(FILE** asFile, char* code, char* line, char* originalLine, int* lineNum){
     char* token;
 
     do {
-        fgets(line, MAX_LINE_LENGTH, *asFile);
+        fgets(line, MAX_LINE_LENGTH, asFile);
         strcpy(originalLine, line);
         token = strtok(line, DELIMITERS);
     }
@@ -27,29 +28,61 @@ int add_macro_codelines(FILE** asFile, char* code, char line[], char originalLin
         if (*token != ';'){ /* can there be any comments in macro definition? */
             code = (char*)realloc(code, sizeof(char) * (strlen(code) + strlen(originalLine) + 1));
             strcat(code, originalLine);
+
             do {
-                fgets(line, MAX_LINE_LENGTH, *asFile);
+                fgets(line, MAX_LINE_LENGTH, asFile);
                 strcpy(originalLine, line);
                 token = strtok(line, DELIMITERS);
             }
             while (token == NULL);
+
+            (*lineNum)++;
         }
     }
-    return 0;
+
+    token = strtok(NULL, DELIMITERS);
+    if (token != NULL)
+        error_handler(Extratanatious_Text_After_Macro_Declaration, lineNum);
+
+    return TRUE;
 }
 
-macro* add_macro_node(macro* head, char* name){
+int error_handler(int errorCode, int lineNum){
+    switch (errorCode){
+    case Extratanatious_Text_After_Macro_Declaration:
+        printf("Error: Extratanatious text after macro declaration on line %d\n", lineNum);
+        break;
+    case Macro_Already_Exists:
+        printf("Error: Macro already exists on line %d\n", lineNum);
+        break;
+    }
+
+    return 1;
+}
+
+
+macro* add_macro_node(macro* head, char** name, int* errorStatus, int lineNum){
     macro* newMacro;
     newMacro = (macro*)malloc(sizeof(macro));
+
+    if (find_macro(*name) == TRUE)
+        *errorStatus = error_handler(Macro_Already_Exists, lineNum); // macro already exists
 
     if (head != NULL)
         head->next = NULL;
 
     newMacro->next = head;
-    newMacro->name = (char*)calloc(strlen(name)+1, sizeof(char));
+    newMacro->name = (char*)calloc(strlen(*name)+1, sizeof(char));
     newMacro->code = (char*)calloc(1, sizeof(char));
 
-    strcpy(newMacro->name, name);
+    *name = strtok(NULL, DELIMITERS);
+    if (*name != NULL)
+        *errorStatus = error_handler(Extratanatious_Text_After_Macro_Declaration, lineNum); // some extra characters after macro declaration
+    
+    if (is_operation() != FALSE || is_guidanceshit() != FALSE || is_guidancelabelshit() != FALSE)
+        *errorStatus = error_handler(Macro_Name_Is_Saved_Word, lineNum); // macro name is a saved word, which isnt right
+        
+    strcpy(newMacro->name, *name);
 
     return newMacro;
 }
@@ -92,39 +125,40 @@ int free_macros(macro* head){
 }
 
 /*handles the input and output file*/
-int preassembler_invoke(FILE** asFile, FILE** amFile, char* filename){
+int preassembler_invoke(char* filename){
+    FILE* asFile; 
+    FILE* amFile;
     macro* head;
     char* token;
     char line[MAX_LINE_LENGTH], originalLine[MAX_LINE_LENGTH];
-    int lineNum;
+    int lineNum, errorFound;
 
     lineNum = 0;
     head = NULL;
     
-    (*amFile) = open_file(filename, ".am", "w");
-    while (read_input_file(asFile, filename, ".as", originalLine, &lineNum)){
+    amFile = open_file(filename, ".am", "w");
+    while (read_input_file(&asFile, filename, ".as", originalLine, &lineNum) == TRUE){
         strcpy(line, originalLine);
         token = strtok(line, DELIMITERS);
+
         if (token != NULL && *token != ';'){
             if (strcmp(token, "mcro") == 0){
                 token = strtok(NULL, DELIMITERS);
-                head = add_macro_node(head, token);
-                add_macro_codelines(asFile, head->code, line, originalLine);
+                head = add_macro_node(head, token, &errorFound, lineNum);
+                add_macro_codelines(&asFile, head->code, line, originalLine, &lineNum);
             }
             else
-                write_line(amFile, head, token, originalLine, line);
+                write_line(&amFile, head, token, originalLine, line);
         }
-    }
-    free_macros(head);
 
-    fclose(*asFile); 
-    fclose(*amFile);
+    }
+
+    free_macros(head);
+    fclose(asFile); 
+    fclose(amFile);
 }
 
 int main(){
-    FILE* asFile;
-    FILE* amFile;
-
-    preassembler_invoke(&asFile, &amFile, "test");
+    preassembler_invoke("test");
     return 0;
 }
--- preassembler.h
+++ preassembler.h
@@ -9,7 +9,13 @@
 #define DELIMITERS " \t\n"
 #define MAX_LINE_LENGTH 82
 #define MAX_FILENAME_LENGTH 30
+
+#define FALSE 0
+#define TRUE 1
 /*macro for freeing all the allocated space in the macros (the pointers attrabiutes and the pointer itself)*/
+
+enum Errors {Extratanatious_Text_After_Macro_Declaration = 0, Macro_Already_Exists, Macro_Name_Is_Saved_Word};
+
 typedef struct macro{
     char* name;
     char* code;
@@ -17,4 +23,11 @@ typedef struct macro{
     struct macro* next;
 } macro;
 
+int preassembler_invoke(char* filename);
+int free_macros(macro* head);
 int read_input_file(FILE** sourceFile, char* filename, char ending[3], char line[MAX_LINE_LENGTH], int* lineNum);
+FILE* open_file(char* filename, char* ending, char* mode);
+macro* add_macro_node(macro* head, char** name, int* errorStatus, int lineNum);
+int error_handler(int errorCode, int lineNum);
+int add_macro_codelines(FILE** asFile, char* code, char* line, char* originalLine, int* lineNum);
+int write_line(FILE** amFile, macro* head, char* token, char originalLine[], char line[])
